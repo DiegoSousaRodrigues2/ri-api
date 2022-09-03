638f38090a57d93511f3650265e55704b5bfa078
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2022-09-03 05:43:46 -0300

    Crud Turma

---
+++ src/main/java/br/com/fiap/riapi/repository/TurmaRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.riapi.repository;
+
+import br.com.fiap.riapi.domains.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<Turma, Integer> {
+
+}
